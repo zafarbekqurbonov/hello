07f32f411ec23acfe30bd1db712707fd22885649
zafarbekqurbonov <zafarbekqurbonov16@gmail.com>
2024-09-09 14:32:46 +0500

    https://github.com/zafarbekqurbonov/hello.git

---
+++ src/uz/pdp/Main.java
@@ -0,0 +1,15 @@
+package uz.pdp;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+    public static Logger logger = Logger.getLogger("my_logger");
+
+    public static void main(String[] args){
+
+        logger.log(Level.INFO,"salom");
+
+
+    }
+}
